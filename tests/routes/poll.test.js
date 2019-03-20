e9320f0fbda623a3977686068cba18a803dedbc2
Aaron Dennis <AMDennis1987@gmail.com>
2019-03-20 10:05:17 -0700

    added auth to routes, tests passing with new email field

--- tests/routes/poll.test.js
+++ tests/routes/poll.test.js
@@ -2,19 +2,23 @@ const request = require('supertest');
 const app  = require('../../lib/app');
 const { getPoll } = require('./../dataHelpers');
 
+jest.mock('../../lib/services/auth.js');
+jest.mock('../../lib/middleware/ensureAuth.js');
+
 
 describe('Test the poll routes', () => {
+
   it('can post a new poll to the database', () => {
     return request(app)
       .post('/polls')
-      .send({ question: 'chicken or beef?', options: ['yes', 'no'], author: 'some author' })
+      .send({ question: 'chicken or beef?', options: ['yes', 'no'], email: 'newemail@email.com' })
       .then(res => {
         expect(res.ok).toBeTruthy();
         expect(res.body).toEqual({ 
           options: ['yes', 'no'],
           _id: expect.any(String),
           question: 'chicken or beef?',
-          author: 'some author',
+          email: '123456@email.com',
           __v: 0 });
       });
   });
@@ -34,10 +38,10 @@ describe('Test the poll routes', () => {
       .then(res => {
         expect(res.ok).toBeTruthy();
         expect(res.body).toEqual({
-          author: 'test.user',
+          email: expect.any(String),
           _id: expect.any(String),
           question: 'My question0',
-          options: ['option1', 'options2'],
+          options: [expect.any(String), expect.any(String), expect.any(String)],
           __v: 0,
         });
       });
